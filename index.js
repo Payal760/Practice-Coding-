bc0eaa54009ab4f1d81520a8334645338d5fbf42
Payal760 <payalardeshana96@gmail.com>
2024-12-21 02:28:01 +0530

    My JSON practice is complete

---
+++ index.js
@@ -0,0 +1,29 @@
+// function addNumber(...even){
+//    let ans = even.reduce((acc,el,i)=>{
+//            return acc+el 
+        
+//    },0)
+// //    console.log(ans) 
+//    let ans2 = ans/even.length
+//    return ans2
+// }
+// console.log(addNumber(3,4,5,6));
+// addNumber(4,4,5)  
+
+
+
+// let arr = [2,3,5,6,7,8]
+// let ans = arr.reduce((acc,el,i)=>{
+//       return acc+el
+// },0)
+// console.log(ans);
+
+
+function multiply(...number){
+    let ans =  
+}
+    let arr1 = [2,4,5,6,7,];
+    let arr2 = [2,4,5,6,7,5,6,7]; 
+
+    let 
+
